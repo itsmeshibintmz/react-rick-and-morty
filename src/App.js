9c001aab7f6bdef9e1caf21a7f6ef719d8afc791
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-12-20 23:19:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "bootstrap/dist/js/bootstrap";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center">Rick & Morty <span className="text-primary">WiKi</span></h1>
+      <h1 className="text-center">
+        Rick & Morty <span className="text-primary">WiKi</span>
+      </h1>
     </div>
   );
 }
